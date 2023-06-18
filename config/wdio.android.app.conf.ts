7b70d4cc6e3ee00a19967f61358c75c578951712
nurahmadf <nurahmadf09@gmail.com>
2023-06-18 18:14:58 +0700

    test

--- config/wdio.android.app.conf.ts
+++ config/wdio.android.app.conf.ts
@@ -1,12 +1,10 @@
-import { join } from 'path';
-import config from './wdio.shared.local.appium.conf';
+import { join } from "path";
+import config from "./wdio.shared.local.appium.conf";
 
 // ============
 // Specs
 // ============
-config.specs = [
-    './tests/specs/**/app*.spec.ts',
-];
+config.specs = ["./tests/specs/**/app*.spec.ts"];
 
 // ============
 // Capabilities
@@ -16,20 +14,23 @@ config.specs = [
 config.capabilities = [
     {
         // The defaults you need to have in your config
-        platformName: 'Android',
+        platformName: "Android",
         maxInstances: 1,
         // For W3C the appium capabilities need to have an extension prefix
         // http://appium.io/docs/en/writing-running-appium/caps/
         // This is `appium:` for all Appium Capabilities which can be found here
-        'appium:deviceName': 'Pixel_3_10.0',
-        'appium:platformVersion': '10.0',
-        'appium:orientation': 'PORTRAIT',
-        'appium:automationName': 'UiAutomator2',
+        "appium:deviceName": "Pixel 6 Pro API 33",
+        "appium:platformVersion": "13.0",
+        "appium:orientation": "PORTRAIT",
+        "appium:automationName": "UiAutomator2",
         // The path to the app
-        'appium:app': join(process.cwd(), './apps/Android-NativeDemoApp-0.4.0.apk'),
+        "appium:app": join(
+            process.cwd(),
+            "./apps/Android-NativeDemoApp-0.4.0.apk"
+        ),
         // @ts-ignore
-        'appium:appWaitActivity': 'com.wdiodemoapp.MainActivity',
-        'appium:newCommandTimeout': 240,
+        "appium:appWaitActivity": "com.wdiodemoapp.MainActivity",
+        "appium:newCommandTimeout": 240,
     },
 ];
 
